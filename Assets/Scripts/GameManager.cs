0da12c40e1c0436456f97645ca0cf03d5602a8ce
max <ttyhgo@gmail.com>
2015-09-01 00:08:41 +0900

    Rogukill test wiriting

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public float levelStartDelay = 2f;
+	public float turnDelay = .1f;
+	public static GameManager instance = null;
+	public BoardManager boardScript;
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
+
+	private Text levelText;
+	private GameObject levelImage;
+	private int level = 1;
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
+	private bool dointSetup;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		enemies = new List<Enemy>() ;
+		boardScript = GetComponent<BoardManager> ();
+		InitGame ();
+	}
+
+	private void OnLevelWasLoaded(int index){
+		level++;
+		InitGame ();
+	}
+
+	void InitGame(){
+		dointSetup = true;
+		levelImage = GameObject.Find ("levelImage");
+		levelText = GameObject.Find("levelText").GetComponent<Text>();
+		levelText.text = 
+		enemies.Clear ();
+		boardScript.SetupScene (level);
+	}
+
+	public void GameOver(){
+		enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playersTurn || enemiesMoving)
+			return;
+		StartCoroutine (MoveEnemies ());
+	}
+
+	public void AddEnemyToList(Enemy script){
+		enemies.Add(script);
+	}
+
+	IEnumerator MoveEnemies(){
+		enemiesMoving = true;
+		yield return new WaitForSeconds(turnDelay);
+		if (enemies.Count == 0) {
+			yield return new WaitForSeconds (turnDelay);
+		}
+		for (int i=0; i<enemies.Count; i++) {
+			enemies [i].MoveEnemy ();
+			yield return new WaitForSeconds (enemies [i].moveTime);
+		}
+
+		playersTurn = true;
+		enemiesMoving = false;
+	}
+}
